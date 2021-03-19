bcb3c5cb5fcc6d54dbb597b1209eb0e97b88c15f
Mikael Holmgren <mikael.holmgren@iths.se>
2021-03-19 09:59:36 +0100

    Added family and list classes

---
+++ ShoppingList/Models/Family.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class Family
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<IdentityUser> Members { get; set; }
+        public string IdentityUserId { get; set; }
+    }
+}
--- ShoppingList/Models/GroceryList.cs
+++ ShoppingList/Models/GroceryList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace ShoppingList.Models
 {
     public class GroceryList
     {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<GroceryItem> Items { get; set; }
+        [Required]
+        public int GroceryItemId { get; set; }
+        [Required]
+        public int FamilyId { get; set; }
+        public Family Family { get; set; }
     }
+
 }
